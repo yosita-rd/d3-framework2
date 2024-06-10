e905034e3ff89b621ba2094f3c28c51171f8922b
popo <yosita.rd@gmail.com>
2024-06-10 14:21:47 +0700

    create map ouutline

--- src/app/page.js
+++ src/app/page.js
@@ -1,105 +1,132 @@
+'use client';
+
 import * as d3 from 'd3';
+import { useRef, useEffect } from 'react';
 
 export default function Home() {
-  const LinePlot = ({
-    data,
-    width = 320,
-    height = 200,
-    marginTop = 20,
-    marginRight = 20,
-    marginBottom = 20,
-    marginLeft = 20,
-  }) => {
-    const x = d3.scaleLinear(
-      [0, data.length - 1],
-      [marginLeft, width - marginRight]
-    );
-    const y = d3.scaleLinear(d3.extent(data), [
-      height - marginBottom,
-      marginTop,
-    ]);
-    const line = d3.line((d, i) => x(i), y); //i = index
-    return (
-      <svg width={width} height={height} className="bg-white">
-        {/* line */}
-        <path
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="1.5"
-          d={line(data)}
-        />
-        {/* circle / <g></g> container used to group other SVG elements */}
-        <g fill="yellow" stroke="currentColor" strokeWidth="1.5">
-          {data.map((d, i) => (
-            <circle key={i} cx={x(i)} cy={y(d)} r="2.5" />
-          ))}
-        </g>
-      </svg>
-    );
+  const svgRef = useRef(null);
+
+  //geographic data from geojson.org.
+  //use pixel scale from figma
+  const GeoJson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        properties: {
+          name: 'Zone1',
+        },
+        geometry: {
+          type: 'Polygon',
+          coordinates: [
+            [
+              [291, 490.56],
+              [320.5, 329.5],
+              [430, 209.5],
+              [583.87, 182.17],
+              [583.87, 490.56],
+              [291, 490.56],
+            ],
+          ],
+        },
+      },
+      {
+        type: 'Feature',
+        properties: {
+          name: 'Zone2',
+        },
+        geometry: {
+          type: 'Polygon',
+          coordinates: [
+            [
+              [291, 490.56],
+              [583.87, 490.56],
+              [583.87, 613.35],
+              [291, 613.35],
+              [291, 490.56],
+            ],
+          ],
+        },
+      },
+      {
+        type: 'Feature',
+        properties: {
+          name: 'Zone3',
+        },
+        geometry: {
+          type: 'Polygon',
+          coordinates: [
+            [
+              [583.87, 384.7],
+              [583.87, 137],
+              [1133, 137],
+              [1102, 260.85],
+              [1243, 260.85],
+              [1243, 384.7],
+              [583.87, 384.7],
+            ],
+          ],
+        },
+      },
+      {
+        type: 'Feature',
+        properties: {
+          name: 'Zone4',
+        },
+        geometry: {
+          type: 'Polygon',
+          coordinates: [
+            [
+              [583.87, 384.7],
+              [1006.59, 384.7],
+              [1006.59, 688.86],
+              [790.5, 688.86],
+              [583.87, 613.35],
+              [583.87, 384.7],
+            ],
+          ],
+        },
+      },
+      {
+        type: 'Feature',
+        properties: {
+          name: 'Zone5',
+        },
+        geometry: {
+          type: 'Polygon',
+          coordinates: [
+            [
+              [1006.59, 688.86],
+              [1006.59, 384.7],
+              [1243, 384.7],
+              [1006.59, 688.86],
+            ],
+          ],
+        },
+      },
+    ],
   };
 
-  const Bubble = ({
-    data,
-    width = 320,
-    height = 200,
-    marginTop = 20,
-    marginRight = 20,
-    marginBottom = 20,
-    marginLeft = 20,
-  }) => {
-    const format = d3.format(',d');
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
+  //projection geo data -> x, y
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
 
-    //pack layout is big circles with smaller circles inside
-    const pack = d3
-      .pack()
-      .size([
-        width - marginLeft - marginRight,
-        height - marginTop - marginBottom,
-      ])
-      .padding(3);
+    const projection = d3.geoIdentity().fitSize([960, 500], GeoJson);
 
-    const root = pack(d3.hierarchy({ children: data }).sum((d) => d.value));
-    return (
-      <svg width={width} height={height} className="bg-white">
-        <g>
-          {root.leaves().map((d, i) => (
-            <g
-              key={i}
-              transform={`translate(${d.x + marginLeft},${d.y + marginTop})`}
-            >
-              <title>{`${d.data.id}\n${format(d.value)}`}</title>
-              <circle r={d.r} fill={color(i)} fillOpacity={0.7} />
-              <text
-                x={0}
-                y={0}
-                textAnchor="middle"
-                fontSize="10px"
-                fill="black"
-              >
-                {`${d.data.id}\n${format(d.value)}`}
-              </text>
-            </g>
-          ))}
-        </g>
-      </svg>
-    );
-  };
+    const path = d3.geoPath().projection(projection);
+
+    svg
+      .selectAll('path')
+      .data(GeoJson.features)
+      .join('path')
+      .attr('d', path)
+      .attr('fill', 'none')
+      .attr('stroke', 'black');
+  }, [GeoJson]);
 
   return (
     <main className="flex flex-row flex-wrap justify-center gap-4 bg-stone-200 min-h-screen p-24">
-      {LinePlot({
-        data: [1, 6, 3, 4, 5, 6, 7, 8, 9, 10],
-      })}
-      {Bubble({
-        data: [
-          { id: 'A', value: 100 },
-          { id: 'B', value: 1200 },
-          { id: 'C', value: 300 },
-          { id: 'D', value: 400 },
-          { id: 'E', value: 1500 },
-        ],
-      })}
+      <svg ref={svgRef} width="960" height="500" viewBox="0 0 960 500"></svg>
     </main>
   );
 }
