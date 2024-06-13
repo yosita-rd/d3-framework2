7724f837d36cf7ce24a63148b62713a1e3f03e4f
popo <yosita.rd@gmail.com>
2024-06-13 13:21:37 +0700

    seperate translate circle and set location function

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,8 @@ import { GeoJson } from '../app/data';
 
 export default function Home() {
   const svgRef = useRef(null);
+
+  //color generator based on the number of danger and warning
   const colorGenerator = (report) => {
     if (report.danger.length > 0) {
       let light = 255 - report.danger.length * 25;
@@ -19,31 +21,38 @@ export default function Home() {
       return 'rgb(215, 215, 215)';
     }
   };
-  //not working yet
-  const zoneAlert = (zone) => {
-    const polygon = zone.geometry;
-    if (d3.geoContains(polygon, currentLocation)) {
-      return 'red';
-    } else {
-      return 'black';
-    }
-  };
 
-  const [currentLocation, setCurrentLocation] = useState(0);
-  //mock path coordinates
-  const pathCoordinates = [
-    [320.5, 329.5],
-    [430, 209.5],
-    [583.87, 182.17],
-    [583.87, 490.56],
-  ];
+  // //not working yet
+  // const zoneAlert = (zone) => {
+  //   const polygon = zone.geometry;
+  //   if (d3.geoContains(polygon, currentLocation)) {
+  //     return 'red';
+  //   } else {
+  //     return 'black';
+  //   }
+  // };
 
-  //projection geo data -> x, y -> svg path
+  //generate random location every 2 seconds
+  const [currentLocation, setCurrentLocation] = useState([0, 0]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let location = [
+        Math.floor(Math.random() * 952),
+        Math.floor(Math.random() * 551.86),
+      ];
+      setCurrentLocation(location);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  //draw the map
   useEffect(() => {
     const svg = d3.select(svgRef.current);
-
-    const projection = d3.geoIdentity().scale(100).fitSize([960, 500], GeoJson);
-
+    const projection = d3
+      .geoIdentity()
+      .scale(100)
+      .fitSize([952, 551.86], GeoJson);
     const path = d3.geoPath(projection);
 
     svg
@@ -51,43 +60,46 @@ export default function Home() {
       .data(GeoJson.features)
       .join('path')
       .attr('d', path)
-      .attr('fill', (d) => colorGenerator(d.properties.report)) //d is the data from GeoJson.features
+      .attr('fill', (d) => colorGenerator(d.properties.report))
       .attr('stroke', 'black');
 
-    // Append the circle once
+    // Append the circle once at [0,0] and hide it
     const circle = svg
       .append('circle')
       .attr('r', 10)
-      .attr('cx', pathCoordinates[0][0])
-      .attr('cy', pathCoordinates[0][1])
+      .attr('cx', 0)
+      .attr('cy', 0)
       .attr('fill', 'blue')
       .attr('hidden', 'true');
   }, []);
 
+  //update the map(fill) and circle every 2 seconds
   useEffect(() => {
     const svg = d3.select(svgRef.current);
 
+    //update the color of the path based on the report
     svg
       .selectAll('path')
-      .attr('fill', (d) => colorGenerator(d.properties.report)) //d is the data from GeoJson.features
-      .attr('stroke', 'black');
-    const circle = svg.select('circle').attr('hidden', null);
+      .attr('fill', (d) => colorGenerator(d.properties.report));
 
-    const nextLocation = (currentLocation + 1) % pathCoordinates.length;
+    // Move the circle to the next location
+    const circle = svg.select('circle').attr('hidden', null);
     circle
       .transition()
       .duration(2000)
-      .attr('cx', pathCoordinates[nextLocation][0])
-      .attr('cy', pathCoordinates[nextLocation][1])
-      .on('end', () => {
-        setCurrentLocation(nextLocation);
-      });
+      .attr('cx', currentLocation[0])
+      .attr('cy', currentLocation[1]);
   }, [currentLocation]);
 
   return (
     <main className="flex flex-row flex-wrap justify-center gap-4 bg-stone-200 min-h-screen p-24">
-      <p>Current Location: {pathCoordinates[currentLocation].join(', ')}</p>
-      <svg ref={svgRef} width="960" height="500" viewBox="0 0 960 500"></svg>
+      <p>Current Location: {currentLocation.join(', ')}</p>
+      <svg
+        ref={svgRef}
+        width="952"
+        height="551.86"
+        viewBox="0 0 952 551.86"
+      ></svg>
     </main>
   );
 }
